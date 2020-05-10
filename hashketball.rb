01b6d3daa9553c25a7b426b1ddbf338c98938a82
stephen pesce <stevepesce879@gmail.com>
2020-05-10 20:51:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -181,7 +181,7 @@ end
 def player_stats(name)
   game = game_hash
   
-  stats = game.reduce({}){|player_stat_hash, (key,val)
+  stats = game.reduce({}){|player_stat_hash, (key,val)|
     if (val[:players][:player_name] == name)
       val[:players].each{|(stat_key, stat_val)|
         if(stat_key != :player_name)
